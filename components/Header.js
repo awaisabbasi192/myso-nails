254a7d86c846e28218391ae5adbf484d009aef40
Awais Madni <awaisabbaxi08@gmail.com>
2026-07-31 01:07:04 +0500

    Add Gifts (gift cards) to header + mobile nav

--- components/Header.js
+++ components/Header.js
@@ -13,6 +13,7 @@ const NAV = [
   { href: "/shop", label: "Shop" },
   { href: "/bridal", label: "Bridal" },
   { href: "/custom", label: "Custom" },
+  { href: "/gift-cards", label: "Gifts" },
   { href: "/faq", label: "FAQ" },
   { href: "/about", label: "About" },
   { href: "/contact", label: "Contact" },
